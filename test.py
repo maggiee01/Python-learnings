265bd2d61ba4102c14b37f78055d169650f61942
maggiee01 <159481847+maggiee01@users.noreply.github.com>
2024-02-09 00:48:26 +0530

    first 2 files upload to github

---
+++ test.py
@@ -0,0 +1,47 @@
+
+# typecasting of variable
+a = str(3)
+b = 5
+c = 'CAD'
+print(a+c)
+
+x = int(2)
+y = str(2)
+z = float(2)
+
+print(x,y,z)
+
+# print to display variables type
+print(type(y))
+
+# assigning multiple values to multiple variables in single line
+x, y, z = 5, 6, 9
+print(x, y, z)
+
+# assigning single value to multiple variables in single line
+x = y = z = 6
+print(x, y, z)
+
+# Example for variables being case sensitive and variable cannot be your keyword or have special chars
+a3 = 7
+print(a3)
+
+A3 = 8
+print(A3)
+
+my_name = "Meghana"  # snake case
+myName = "Meghana"   # camel case
+MyName = "Meghana"   # pastel case
+
+x = "I"
+y = "am"
+z = "learning python"
+print(x,y,z)
+print(x+y+z)     # concatination of strings, prints without spaces
+
+# data types
+
+a = [2, 9, 8, "as"]
+print(a)
+
+
